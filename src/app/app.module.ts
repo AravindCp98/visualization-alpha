3aa87354f8f08df1d30ff598dc3f44c59e0a00f8
AravindCp98 <117508898+AravindCp98@users.noreply.github.com>
2023-03-03 12:34:40 +0530

    :hammer: unwanted module removed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,8 @@ import { RouterModule } from '@angular/router';
 import { ApiCheckerComponent } from './apicheckerModule/api-checker/api-checker.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-// import { MatSelectModule } from '@angular/material/select';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-// import { MatDividerModule } from '@angular/material/divider';
+
 
 @NgModule({
   declarations: [
@@ -24,9 +23,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     HttpClientModule,
     BrowserModule,
     AppRoutingModule,
-    MatSelectModule,
     BrowserAnimationsModule,
-    MatDividerModule,
     RouterModule.forRoot([
       { path: 'apitester', component: ApiCheckerComponent }
     ]),
