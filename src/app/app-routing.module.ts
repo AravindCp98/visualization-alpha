4a76c2064247e00d167eb26f0cb5e4e025f0d7e2
Aravind cp <117508898+AravindCp98@users.noreply.github.com>
2023-05-02 11:11:33 +0000

    :zap: lazyLoad implementation in angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,19 @@ import { ApiCheckerComponent } from './apicheckerModule/api-checker/api-checker.
 import { MainDashBoardComponent } from './main-dash-board/main-dash-board.component';
 import { CoronavirusUpdateComponent } from './coronavirus-update/coronavirus-update.component';
 import { DrillModuleComponent } from './drill-module/drill-module.component';
-
+// import {drillModule} from './drill-module';
 
 const routes: Routes = [
   { path: 'apiChecker', component: ApiCheckerComponent },
   { path: 'visualization-alpha', component: MainDashBoardComponent },
   { path: 'CoronavirusUpdateComponent', component: CoronavirusUpdateComponent },
-  { path: 'Drill', component: DrillModuleComponent }
+  // { path: 'Drill', component: DrillModuleComponent }
+  //implementing lazy loading
+  //if below lazymodule show error then comment it (line 15-17 and uncomment line 13)
+  { path:'Drill',loadChildren:()=> 
+  import('.././app/drill-module').then(module=>module.DrillModuleComponent)
+}
+  
 ];
 
 @NgModule({
