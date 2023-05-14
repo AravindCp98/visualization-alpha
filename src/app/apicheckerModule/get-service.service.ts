47f15ff48bea5b50821755d7a8c4ed09e8ccc8b6
AravindCp98 <aravindcp14@gmail.com>
2023-05-14 21:48:11 +0530

    💡cleanup

--- src/app/apicheckerModule/get-service.service.ts
+++ src/app/apicheckerModule/get-service.service.ts
@@ -9,20 +9,7 @@ import { Observable, catchError, observable, throwError } from 'rxjs';
 export class GetServiceService {
 
   constructor(private httpClient: HttpClient) { }
-  private handleError(error: HttpErrorResponse) {
-    if (error.status === 0) {
-      console.error('An error occurred:', error.error);
-    } else {
-      console.error(
-        `Service side error ${error.status}, body was: `, error.error);
-    }
-    return throwError(() => new Error('Something bad happened; please try again later.'));
-  }
   
-//  async fetchApi(url: string) Observable<any> {
-//     return this.httpClient.get(url);
-//   }
-
 async fetchApi(url:string):Promise<Observable<any>>{
   console.log('fecting started')
   return this.httpClient.get(url)
